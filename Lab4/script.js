df62842d85e16382b5cb47d23ab21372bf57f53b
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-09-16 14:49:33 +0200

    Lab4. Second task implementation (buttons)

--- Lab4/script.js
+++ Lab4/script.js
@@ -11,3 +11,34 @@ clickableElement2.addEventListener("click", () => {
   clickableElement2.style.backgroundColor = "yellowgreen";
   clickableElement2.style.color = "black";
 });
+
+const imageBlock = document.querySelector(".author-favorite-city-photo");
+
+const addButton = document.querySelector(".addButton");
+addButton.addEventListener("click", () => {
+  imageBlock.style.display = "block";
+});
+
+const deleteButton = document.querySelector(".deleteButton");
+deleteButton.addEventListener("click", () => {
+  imageBlock.style.display = "none";
+});
+
+let imgWidth = 350;
+const image = document.querySelector(".image");
+const zoomInButton = document.querySelector(".zoomInButton");
+zoomInButton.addEventListener("click", () => {
+  imgWidth += 100;
+  image.style.width = imgWidth + "px";
+  image.style.height = "auto";
+});
+
+const zoomOutButton = document.querySelector(".zoomOutButton");
+zoomOutButton.addEventListener("click", () => {
+  if (imgWidth > 50) {
+    imgWidth -= 100;
+    image.style.width = imgWidth + "px";
+    image.style.height = "auto";
+    console.log(imgWidth);
+  }
+});
