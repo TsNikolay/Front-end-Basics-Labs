f1f7a9bfd3ce63794af1626dcc4bf0cf62f13ea0
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-09-19 17:09:44 +0200

    Lab5. Minor fixes for data output

--- Lab5/firstTaskScript.js
+++ Lab5/firstTaskScript.js
@@ -20,7 +20,7 @@ function checkInputs() {
   const patterns = {
     fullName: /^.+ [A-Za-zА-Яа-яЁҐЄІЇ]\.[A-Za-zА-Яа-я]\.$/,
     idCard: /^[A-Za-zА-Яа-яЁҐЄІЇ]{2} №\d{6}$/,
-    faculty: /^[A-Za-zА-Яа-яЁҐЄІЇ]+$/,
+    faculty: /^[A-Za-zА-Яа-яЁҐЄІЇєії]+$/,
     birthday: /^\d{2}\.\d{2}\.\d{4}$/,
     address: /^м\.\s[A-ZА-ЯЁҐЄІЇ][a-zа-яёєії]*$/,
   };
@@ -34,19 +34,19 @@ function checkInputs() {
   };
 
   checkInput(patterns.fullName, fullName, classesNames.fullName);
-  checkInput(patterns.idCard, idCard, classesNames).idCard;
+  checkInput(patterns.idCard, idCard, classesNames.idCard);
   checkInput(patterns.faculty, faculty, classesNames.faculty);
   checkInput(patterns.birthday, birthday, classesNames.birthday);
   checkInput(patterns.address, address, classesNames.address);
 }
 
 function checkInput(pattern, enteredData, className) {
-  const resultLineValue = document.querySelector(className).textContent;
+  const resultLineValue = document.querySelector(className);
 
   if (pattern.test(enteredData.value)) {
-    if (resultLineValue.includes(enteredData.value)) {
+    if (resultLineValue.textContent.includes(enteredData.value)) {
     } else {
-      resultLineValue += enteredData.value;
+      resultLineValue.textContent += enteredData.value;
     }
     enteredData.style.color = "#00FF00";
   } else {
